9d51868a92b679ee5bddc78ec3ff3a2f041950e9
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2023-08-05 01:19:11 +0300

    update game field coordinates

--- src/GameField.java
+++ src/GameField.java
@@ -1,5 +1,5 @@
 public class GameField {
-    private static String[][] field = new String[10][10];
+    private static String[][] field = new String[11][11];
 
     public static void setField(String[][] field) {
         GameField.field = field;
@@ -10,8 +10,8 @@ public class GameField {
     }
 
     public static String[][] createField() {
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field.length; j++) {
+        for (int i = 1; i < field.length; i++) {
+            for (int j = 1; j < field.length; j++) {
                 field[i][j] = "☐";
             }
         }
@@ -20,9 +20,9 @@ public class GameField {
 
     public static void printField(String[][] fieldToPrint) {
         System.out.println();
-        for (String[] strings : fieldToPrint) {
-            for (int j = 0; j < fieldToPrint.length; j++) {
-                System.out.print(strings[j] + "\t");
+        for (int i = 1; i < fieldToPrint.length; i++) {
+            for (int j = 1; j < fieldToPrint.length; j++) {
+                System.out.print(fieldToPrint[i][j] + "\t");
             }
             System.out.println();
         }
