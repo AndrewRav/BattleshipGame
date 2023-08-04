69ff1efdb976209db4f0c2df85f35b92886deba4
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2023-08-05 02:52:14 +0300

    test demo version of method fillField

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[][] gameField = GameField.createField();
-        GameField.printField(gameField);
+        Random random = new Random();
+        String[][] gameFieldPlayer1 = GameField.createField();
+        String[][] gameFieldPlayer2 = GameField.createField();
 
+        System.out.println("Давайте расставлять корабли. Начинает ПЕРВЫЙ игрок. Другой игрок, не смотри!");
+        GameField.fillField(gameFieldPlayer1);
 
+        System.out.println("Теперь расставляет корабли ВТОРОЙ игрок. Другой игрок, не смотри!");
+        GameField.fillField(gameFieldPlayer2);
+
+        GameField.printField(gameFieldPlayer1);
+        GameField.printField(gameFieldPlayer2);
     }
 }
