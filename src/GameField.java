4d73c6b6543fcc3bf39660f7baf5823b52269388
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2023-08-05 02:51:10 +0300

    add demo version of method fillField

--- src/GameField.java
+++ src/GameField.java
@@ -1,15 +1,9 @@
+import java.util.Scanner;
+
 public class GameField {
-    private static String[][] field = new String[11][11];
-
-    public static void setField(String[][] field) {
-        GameField.field = field;
-    }
-
-    public static String[][] getField() {
-        return field;
-    }
 
     public static String[][] createField() {
+        String[][] field = new String[11][11];
         for (int i = 1; i < field.length; i++) {
             for (int j = 1; j < field.length; j++) {
                 field[i][j] = "☐";
@@ -25,6 +19,71 @@ public class GameField {
                 System.out.print(fieldToPrint[i][j] + "\t");
             }
             System.out.println();
+        }
+    }
+
+    public static void fillField(String[][] gameField) {
+        Scanner scanner = new Scanner(System.in);
+        String coordinates;
+        String[] coordinatesArray, coords;
+        int counter = 1;
+        String numberName = "первого";
+
+        System.out.println("Координаты для четырёхпалубного корабля: ");
+        coordinates = scanner.nextLine();
+        coordinatesArray = coordinates.split(";");
+        for (String coordsString : coordinatesArray) {
+            coords = coordsString.split(",");
+            gameField[Integer.parseInt(coords[0])][Integer.parseInt(coords[1])] = "\uD83D\uDEA2"; // добавление корабля
+        }
+
+        while (counter <= 2) {
+            if (counter == 2) {
+                numberName = "второго";
+            }
+            System.out.println("Координаты для " + numberName + " трёхпалубного корабля: ");
+            coordinates = scanner.nextLine();
+            coordinatesArray = coordinates.split(";");
+            for (String coordsString : coordinatesArray) {
+                coords = coordsString.split(",");
+                gameField[Integer.parseInt(coords[0])][Integer.parseInt(coords[1])] = "\uD83D\uDEA2"; // добавление корабля
+            }
+            counter++;
+        }
+
+        counter = 1;
+        while (counter <= 3) {
+            switch (counter) {
+                case 1 -> numberName = "первого";
+                case 2 -> numberName = "второго";
+                case 3 -> numberName = "третьего";
+            }
+            System.out.println("Координаты для " + numberName + " двухпалубного корабля: ");
+            coordinates = scanner.nextLine();
+            coordinatesArray = coordinates.split(";");
+            for (String coordsString : coordinatesArray) {
+                coords = coordsString.split(",");
+                gameField[Integer.parseInt(coords[0])][Integer.parseInt(coords[1])] = "\uD83D\uDEA2"; // добавление корабля
+            }
+            counter++;
+        }
+
+        counter = 1;
+        while (counter <= 4) {
+            switch (counter) {
+                case 1 -> numberName = "первого";
+                case 2 -> numberName = "второго";
+                case 3 -> numberName = "третьего";
+                case 4 -> numberName = "четвёртого";
+            }
+            System.out.println("Координаты для " + numberName + " однопалубного корабля: ");
+            coordinates = scanner.nextLine();
+            coordinatesArray = coordinates.split(";");
+            for (String coordsString : coordinatesArray) {
+                coords = coordsString.split(",");
+                gameField[Integer.parseInt(coords[0])][Integer.parseInt(coords[1])] = "\uD83D\uDEA2"; // добавление корабля
+            }
+            counter++;
         }
     }
 
