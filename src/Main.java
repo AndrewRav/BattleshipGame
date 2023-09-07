e9a3a7c7590b0a28c1ec702d2855bd2d554ef401
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2023-09-08 02:10:25 +0300

    clean up

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        String[][] gameFieldPlayer1 = GameField.createField();
-        String[][] gameFieldPlayer2 = GameField.createField();
-
-        Scanner scanner = new Scanner(System.in);
-        String coordinates = scanner.nextLine();
-        Ship twoDeck = new Ship(coordinates);
-        twoDeck.createShip(gameFieldPlayer1);
-
-        GameField.printField(gameFieldPlayer1);
-
-//        System.out.println("Давайте расставлять корабли. Начинает ПЕРВЫЙ игрок. Другой игрок, не смотри!");
-//        GameField.fillField(gameFieldPlayer1);
-//
-//        System.out.println("Теперь расставляет корабли ВТОРОЙ игрок. Другой игрок, не смотри!");
-//        GameField.fillField(gameFieldPlayer2);
-//
-//        GameField.printField(gameFieldPlayer1);
-//        GameField.printField(gameFieldPlayer2);
+        Gameplay.startGame();
     }
 }
