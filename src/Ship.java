13d9bb9d7bc56e5043a787e2d9efdf5cc9f29963
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2023-09-11 02:44:32 +0300

    add ship validation

--- src/Ship.java
+++ src/Ship.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ship {
     private static int deck;
     private String[] coordinatesArray;
@@ -16,6 +19,7 @@ public class Ship {
     }
 
     public void createShip(String[][] gameField, int requiredDeckNumber) throws InputCoordinatesException {
+        ArrayList<Integer> deckCoordinatesList = new ArrayList<>();
         // Проверка введённых координат
         if (requiredDeckNumber != deck) {
             throw new InputCoordinatesException("Вы ввели неверное число палуб, попробуйте ещё раз");
@@ -25,6 +29,8 @@ public class Ship {
         for (String deckCoordinate : coordinatesArray) {
             x = Integer.parseInt(deckCoordinate.split(",")[0]);
             y = Integer.parseInt(deckCoordinate.split(",")[1]);
+            deckCoordinatesList.add(x);
+            deckCoordinatesList.add(y);
 
             if ((x < 0 && y < 0) || (x > 9 && y > 9)) {
                 throw new InputCoordinatesException("Вы ввели число из неверного диапазона (т. е. либо меньше 0, либо больше 9)");
@@ -40,6 +46,24 @@ public class Ship {
                 throw new InputCoordinatesException("Место, в которую вы хотите поставить палубу, уже занято");
             }
         }
+        // Валидация кораблей
+        int temp = deckCoordinatesList.get(0);
+        for (int i = 1; i < deckCoordinatesList.size(); i++) {
+            if (i % 2 == 0) {
+                if (temp + 1 != deckCoordinatesList.get(i) && temp - 1 != deckCoordinatesList.get(i)) {
+                    throw new InputCoordinatesException("Вы ставите палубы не последовательно! Внимательно попробуйте ещё раз");
+                } else {
+                    temp = deckCoordinatesList.get(i);
+                }
+            } else {
+                if (temp + 1 != deckCoordinatesList.get(i) && temp - 1 != deckCoordinatesList.get(i)) {
+                    throw new InputCoordinatesException("Вы ставите палубы не последовательно! Внимательно попробуйте ещё раз");
+                } else {
+                    temp = deckCoordinatesList.get(i);
+                }
+            }
+        }
+
         // Добавление ореол вокруг палуб
         for (String deckCoordinate : coordinatesArray) {
             x = Integer.parseInt(deckCoordinate.split(",")[0]);
