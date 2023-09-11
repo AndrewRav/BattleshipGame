47776fa295695aed2050dd25bce9fb2a00b05b7f
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2023-09-12 00:05:55 +0300

    update processing coordinates

--- src/Ship.java
+++ src/Ship.java
@@ -2,12 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ship {
-    private static int deck;
+    private int deck;
     private String[] coordinatesArray;
+    private ArrayList<Integer> deckCoordinatesList = new ArrayList<>();
 
     public Ship(String coordinates) {
         this.coordinatesArray = coordinates.split(";"); // [1,2]; [3,4]; [5,6] ...
         this.deck = coordinatesArray.length;
+        for (String deckCoordinate : coordinatesArray) {
+            deckCoordinatesList.add(Integer.parseInt(deckCoordinate.split(",")[0]));
+            deckCoordinatesList.add(Integer.parseInt(deckCoordinate.split(",")[1]));
+        }
     }
 
     public int getDeck() {
@@ -18,8 +23,11 @@ public class Ship {
         return coordinatesArray;
     }
 
+    public ArrayList<Integer> getDeckCoordinatesList() {
+        return deckCoordinatesList;
+    }
+
     public void createShip(String[][] gameField, int requiredDeckNumber) throws InputCoordinatesException {
-        ArrayList<Integer> deckCoordinatesList = new ArrayList<>();
         // Проверка введённых координат
         if (requiredDeckNumber != deck) {
             throw new InputCoordinatesException("Вы ввели неверное число палуб, попробуйте ещё раз");
@@ -29,15 +37,13 @@ public class Ship {
         for (String deckCoordinate : coordinatesArray) {
             x = Integer.parseInt(deckCoordinate.split(",")[0]);
             y = Integer.parseInt(deckCoordinate.split(",")[1]);
-            deckCoordinatesList.add(x);
-            deckCoordinatesList.add(y);
 
             if ((x < 0 && y < 0) || (x > 9 && y > 9)) {
                 throw new InputCoordinatesException("Вы ввели число из неверного диапазона (т. е. либо меньше 0, либо больше 9)");
             }
 
             if (deckCoordinate.split(",").length != 2) {
-                throw new InputCoordinatesException("Вы ввели неверное количество координат. Напоминаем формат: x,y;x,y;x,y...]");
+                throw new InputCoordinatesException("Вы ввели неверное количество координат. Напоминаем формат: x,y;x,y;x,y...");
             }
 
             if (gameField[x][y].equals("☐")) {
@@ -46,7 +52,7 @@ public class Ship {
                 throw new InputCoordinatesException("Место, в которую вы хотите поставить палубу, уже занято");
             }
         }
-        // Валидация кораблей
+        // Проверка кораблей
         int temp = deckCoordinatesList.get(0);
         for (int i = 1; i < deckCoordinatesList.size(); i++) {
             if (i % 2 == 0) {
